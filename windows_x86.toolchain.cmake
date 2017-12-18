feb2d0209c4a7a0ae983dd696a2f2b5a2d9ece1a
李樟取 <lizhangqu@weidian.com>
2017-12-18 11:07:07 +0800

    * 32位windows工具链cmake脚本

--- windows_x86.toolchain.cmake
+++ windows_x86.toolchain.cmake
@@ -1,22 +1,22 @@
 # sudo apt-get install mingw-w64
-# windows 64位操作系统
-# -DCMAKE_TOOLCHAIN_FILE=/path/to/windows.toolchain.cmake
+# windows 32位操作系统
+# -DCMAKE_TOOLCHAIN_FILE=/path/to/windows_x86.toolchain.cmake
 
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_VERSION 1)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_SYSTEM_PROCESSOR x86)
 
-set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-SET(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
-set(CMAKE_AR           x86_64-w64-mingw32-ar CACHE FILEPATH "Archiver")
-set(CMAKE_RANLIB       x86_64-w64-mingw32-ranlib CACHE FILEPATH "Ranlib")
-set(CMAKE_ASM_COMPILER x86_64-w64-mingw32-as)
-set(CMAKE_LINKER       x86_64-w64-mingw32-ld)
-set(CMAKE_NM           x86_64-w64-mingw32-nm)
-set(CMAKE_OBJCOPY      x86_64-w64-mingw32-objcopy)
-set(CMAKE_OBJDUMP      x86_64-w64-mingw32-objdump)
-set(CMAKE_STRIP        x86_64-w64-mingw32-strip)
+set(CMAKE_C_COMPILER   i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER  i686-w64-mingw32-windres)
+set(CMAKE_AR           i686-w64-mingw32-ar CACHE FILEPATH "Archiver")
+set(CMAKE_RANLIB       i686-w64-mingw32-ranlib CACHE FILEPATH "Ranlib")
+set(CMAKE_ASM_COMPILER i686-w64-mingw32-as)
+set(CMAKE_LINKER       i686-w64-mingw32-ld)
+set(CMAKE_NM           i686-w64-mingw32-nm)
+set(CMAKE_OBJCOPY      i686-w64-mingw32-objcopy)
+set(CMAKE_OBJDUMP      i686-w64-mingw32-objdump)
+set(CMAKE_STRIP        i686-w64-mingw32-strip)
 
 message(STATUS "CMAKE_C_COMPILER = ${CMAKE_C_COMPILER}")
 message(STATUS "CMAKE_CXX_COMPILER = ${CMAKE_CXX_COMPILER}")
@@ -72,7 +72,7 @@ set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS}")
 
 
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLYONLY)
